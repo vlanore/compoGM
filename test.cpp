519147bc2cb73c5e7a812570c41bbf3d679ba484
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-02-21 14:18:51 +0100

    Connect and set now working (with almost no unnecessary syntax)

--- test.cpp
+++ test.cpp
@@ -5,39 +5,36 @@
 
 
 // ######################################
+//                INTERFACES
+// ######################################
+class Message {
+public:
+    virtual std::string getMessage() = 0;
+};
+
+
+// ######################################
 //                INSTANCES
 // ######################################
 class Instance {
 public:
-    std::shared_ptr<Instance> Out;
     virtual void hello() { printf("Base class.\n"); }
 };
 
-class SuperInstance : public Instance {
+class SuperInstance : public Instance, public Message {
 public:
     std::string name;
     int size;
+    Message* Out;
     SuperInstance(std::string name, int size) : name(name), size(size) {}
     void hello() override {
         printf("Child class: %s(%d).\n", name.c_str(), size);
-        neighbour();
+        if (Out != nullptr)
+            printf("<Neighbour: %s>\n", Out->getMessage().c_str());
     }
-
-    void neighbour() {
-        if (Out != nullptr) {
-            printf("Neighbour: ");
-            Out->hello();
-        }
+    std::string getMessage() override {
+        return "Hello!";
     }
-};
-
-
-// ######################################
-//                INTERFACES
-// ######################################
-class Interface {
-public:
-
 };
 
 
@@ -60,9 +57,16 @@ public:
         instances.emplace(name, std::make_shared<T>(std::forward<Args>(args)...));
     }
 
-    void connect(std::string i1, std::string i2) {
-        instances[i1]->Out = instances[i2];
+    template <class I, typename V>
+    void set(std::string name, V I::* member, V value) {
+        dynamic_cast<I*>(instances[name].get())->*member = value;
     }
+
+    template <class O, class D>
+    void connect(std::string i1, std::string i2, D* O::* member) {
+        set<O,D*>(i1, member, dynamic_cast<D*>(instances[i2].get()));
+    }
+
 };
 
 
@@ -75,7 +79,8 @@ int main() {
     mymodel.instantiate<Instance>("I1");
     mymodel.instantiate<SuperInstance>("I2", "alice", 7);
     mymodel.instantiate<SuperInstance>("I3", "bob", 8);
-    mymodel.connect("I2", "I3");
+    mymodel.set<SuperInstance, int>("I2", &SuperInstance::size, 37);
+    mymodel.connect<SuperInstance, Message>("I2", "I3", &SuperInstance::Out);
 
     mymodel.print_all();
 
