cb42eb68f026180c652c13e2e6be863d34b09dbf
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-02-21 15:42:28 +0100

    Added intproxy class to test

--- test.cpp
+++ test.cpp
@@ -4,16 +4,16 @@
 // ######################################
 //                INTERFACES
 // ######################################
-class Message {
+class GetInt {
 public:
-    virtual std::string getMessage() = 0;
+    virtual int getInt() = 0;
 };
 
 
 // ######################################
 //                INSTANCES
 // ######################################
-class I_Int : public Instance {
+class I_Int : public Instance, public GetInt {
 public:
     int val;
 
@@ -21,6 +21,20 @@ public:
 
     void hello() override {
         printf("I_Int(%d)", val);
+    }
+
+    int getInt() override { return val; }
+};
+
+class I_IntProxy : public Instance {
+public:
+    GetInt* use;
+
+    void hello() override {
+        if (use != nullptr)
+            printf("I_IntProxy(%d)", use->getInt());
+        else
+            printf("I_IntProxy(INVALID)");
     }
 };
 
@@ -31,12 +45,14 @@ public:
 int main() {
     Assembly mymodel;
 
-    mymodel.instantiate<Instance>("I1");
-    mymodel.instantiate<I_Int>("I2", 7);
-    mymodel.instantiate<Array<I_Int> >("I3", 5, [](int i) { return I_Int(i); });
-    mymodel.instantiate<I_Int>("I4", 8);
+    mymodel.instantiate <Instance> ("I1");
+    mymodel.instantiate <I_IntProxy> ("I2");
+    mymodel.instantiate <Array<I_Int> > ("I3", 5, [](int i) { return I_Int(2*i); });
+    mymodel.instantiate <I_Int> ("I4", 8);
 
-    mymodel.set<I_Int, int>("I2", &I_Int::val, 37);
+    mymodel.connect<UseProvide<I_IntProxy, GetInt> >("I2", "I4", &I_IntProxy::use);
+
+    mymodel.set<I_Int, int>("I4", &I_Int::val, 37);
 
     mymodel.print_all();
 
