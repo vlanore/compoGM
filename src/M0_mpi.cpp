8ffafa82c5d8d4aaffb5822114359d9133fe9f29
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-10-09 17:22:17 +0200

    Grouped mpi-related headers into a compoGM_mpi header.

--- src/M0_mpi.cpp
+++ src/M0_mpi.cpp
@@ -25,10 +25,7 @@ more generally, to use and operate it in the same conditions as regards security
 The fact that you are presently reading this means that you have had knowledge of the CeCILL-C
 license and that you accept its terms.*/
 
-#include "compoGM.hpp"
-#include "mpi_helpers.hpp"
-#include "mpi_move_set.hpp"
-#include "mpi_proxies.hpp"
+#include "compoGM_mpi.hpp"
 
 using namespace std;
 using namespace compoGM;
