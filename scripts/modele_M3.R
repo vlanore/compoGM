a1907aaf3d8e89387e6d892e42b28e81b3bd6830
Philippe Veber <philippe.veber@gmail.com>
2018-10-17 16:45:55 +0200

    scripts: refactored and fixed M3

--- scripts/modele_M3.R
+++ scripts/modele_M3.R
@@ -1,27 +1,5 @@
 ## run with Rscript model_M2.R <input-directory> <output-directory>
 
-library(rjags)
-
-args <- commandArgs(trailingOnly=TRUE)
-input_dir <- args[1]
-dest  <- args[2]
-n.chains <- 3
-
-counts <- read.table(paste0(input_dir, "/counts.tsv"), header = T, sep = "\t")
-samples <- read.table(paste0(input_dir, "/samples.tsv"), header = T, sep = "\t")
-size_factors <- read.table(paste0(input_dir, "/size_factors.tsv"), header = T, sep = "\t")
-
-data4jags <- function(genesel, samplesel) {
-    list(
-      sf = size_factors[samplesel,2],
-      ngenes = length(genesel),
-      nsamples = length(samplesel),
-      K = counts[genesel, 1 + samplesel],
-      ncond = length(unique(samples[samplesel,2])),
-      cond = as.integer(samples[samplesel,2])
-    )
-}
-
 model <- "model {
   for(i in 1:ngenes) {
     for(j in 1:nsamples) {
@@ -33,40 +11,18 @@ model <- "model {
 
   for(i in 1:ngenes) {
     for(k in 1:ncond) {
-      log10_q[i, k] ~ dnorm(3, 1 / (1.5 ** 2))
+      log10_q[i, k] ~ dnorm(2, 1 / (2 ** 2))
     }
   }
 
   for(i in 1:ngenes) {
-    q_bar[i] <- sum(10 ** log10_q[i,1:ncond]) / ncond
     alpha[i] <- 10 ** log10_alpha[i]
-    log10_alpha[i] ~ dnorm(log(alpha_bar[i])/log(10)), 1 / sigma_alpha ** 2)
+    log10_alpha[i] ~ dnorm(log(alpha_bar[i]) / log(10), 1 / sigma_alpha ** 2)
     alpha_bar[i] <- a0 + a1 / q_bar[i]
+    q_bar[i] <- sum(10 ** log10_q[i,1:ncond]) / ncond
   }
   sigma_alpha ~ dexp(1)
   a0 ~ dlnorm(-2, 1/2**2)
   a1 ~ dlnorm(0, 1/2**2)
 }
 "
-
-
-run_mcmc <- function(genesel, samplesel) {
-    mcmc <- jags.model(textConnection(model),
-                       data = data4jags(genesel, samplesel),
-                       n.chains = n.chains)
-    update(mcmc, 5000)
-    sample <- coda.samples(mcmc,
-                           variable.names = c("log10_q", "alpha", "sigma_alpha", "log10_a", "log10_b"),
-                           n.iter = 5000)
-    sample
-}
-
-save_trace <- function(trace) {
-  system(paste0("mkdir -p ", dest))
-  for(i in 1:n.chains) {
-    write.table(trace[[i]],file = paste0(dest,"/trace.",i,".tsv"), sep = "\t", quote = F, row.names = F)
-  }
-}
-
-trace <- run_mcmc(1:4, 1:dim(samples)[1])
-save_trace(trace)
