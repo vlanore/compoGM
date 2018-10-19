9fcba7ef5d7ff8f3621b2bf6d67f2da9b0e2feaf
Philippe Veber <philippe.veber@gmail.com>
2018-10-19 15:27:48 +0200

    scripts: added script for effectiveSize calculation

---
+++ scripts/effectiveSize.R
@@ -0,0 +1,10 @@
+# Rscript effectiveSize.R <input> > <output>
+library(coda)
+
+args <- commandArgs(trailingOnly=TRUE)
+input <- args[1]
+output <- args[2]
+d <- read.table(input, header = T, sep = '\t')
+r <- effectiveSize(mcmc(d))
+write.table(r, file = output, row.names = T, col.names = F, sep = '\t', quote=F)
+
